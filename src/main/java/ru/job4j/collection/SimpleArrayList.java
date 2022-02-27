40d91cc7d9cdcacdac9d9ae4101a5566a25adf53
axelmark <axelmark@mail.ru>
2022-02-27 22:19:28 +0300

    1. Динамический список на массиве. [#158]

--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -1,17 +1,16 @@
 package ru.job4j.collection;
 
-import ru.job4j.list.List;
-
 import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import java.util.Objects;
+import ru.job4j.list.List;
 
 public class SimpleArrayList<T> implements List<T> {
 
     private T[] container;
     private int size;
-    private int modCount = 0;
+    private int modCount;
 
     public SimpleArrayList(int capacity) {
         this.container = (T[]) new Object[capacity];
@@ -19,54 +18,67 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        container[modCount++] = value;
+        if (size >= container.length) {
+            container = Arrays.copyOf(container, container.length * 2);
+        }
+        container[size++] = value;
+        modCount++;
     }
 
     @Override
     public T set(int index, T newValue) {
-        return null;
+        if (index < 0 || index >= container.length) {
+            throw new IndexOutOfBoundsException();
+        }
+        T edited = container[index];
+        container[index] = newValue;
+        return edited;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        T removed = container[index];
+        System.arraycopy(container, index + 1, container, index, container.length - index - 1);
+        container[container.length - 1] = null;
+        modCount++;
+        this.size--;
+        return removed;
     }
 
     @Override
     public T get(int index) {
-        if (Objects.checkIndex(index, container.length) == index) {
+        if (index < 0 || index >= container.length) {
             throw new IndexOutOfBoundsException();
         }
-
         return container[index];
     }
 
     @Override
     public int size() {
-        return container.length;
+        return this.size;
     }
 
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
+            private final int expectedModCount = modCount;
+            public int i = 0;
 
             @Override
             public boolean hasNext() {
-                return true;
+                return i < container.length && container[i] != null && size - i != 0;
             }
 
             @Override
             public T next() {
-                return null;
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[i++];
             }
         };
-    }
-
-    public static void main(String[] args) {
-        SimpleArrayList<Integer> list = new SimpleArrayList<>(2);
-        list.add(1);
-        list.add(2);
-
-        System.out.println(list.get(1));
     }
 }
