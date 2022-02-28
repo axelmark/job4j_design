1903f81414397425bb535c0ccee13b377ce25f09
axelmark <axel@mail.ru>
2022-02-28 17:58:16 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/list/List.java
@@ -0,0 +1,7 @@
+package ru.job4j.collection.list;
+
+public interface List<E> extends Iterable<E> {
+    void add(E value);
+
+    E get(int index);
+}
---
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection.list;
+
+import java.util.Iterator;
+
+public class SimpleLinkedList<E> implements List<E> {
+    private int modCount;
+
+    @Override
+    public void add(E value) {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
