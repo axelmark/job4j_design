65c098d43d126a849650b7b7540ac8999ea47f4f
axelmark <axel@mail.ru>
2022-03-04 14:46:42 +0300

    2. Создать контейнер на базе связанного списка [#159 #270416]

--- src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -1,27 +1,25 @@
 package ru.job4j.collection.list;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class SimpleLinkedList<E> implements List<E> {
-
-    private Node<E> first = null;
-    private Node<E> last = null;
-
+    private Node<E> first;
+    private Node<E> last;
     private int modCount;
     private int size;
 
     @Override
     public void add(E value) {
-        Node<E> newNode = new Node<E>(null, value, null);
-
-        if (first == null) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, value, null);
+        last = newNode;
+        if (l == null)
             first = newNode;
-        } else {
-            last = newNode.prev = first;
-            first.next = last;
-        }
-
+        else
+            l.next = newNode;
         size++;
         modCount++;
     }
@@ -29,11 +27,14 @@ public class SimpleLinkedList<E> implements List<E> {
     @Override
     public E get(int index) {
         Objects.checkIndex(index, size);
-        return null;
+        SimpleLinkedList.Node<E> cur = first;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.item;
     }
 
     private static class Node<E> {
-
         E item;
         SimpleLinkedList.Node<E> next;
         SimpleLinkedList.Node<E> prev;
@@ -47,12 +48,25 @@ public class SimpleLinkedList<E> implements List<E> {
 
     @Override
     public Iterator<E> iterator() {
-        return null;
-    }
+        return new Iterator<E>() {
+            private final int expectedModCount = modCount;
+            public int i = 0;
 
-    public static void main(String[] args) {
-        SimpleLinkedList<Integer> ls = new SimpleLinkedList<>();
-        ls.add(1);
-        ls.add(2);
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return i < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return get(i++);
+            }
+        };
     }
 }
