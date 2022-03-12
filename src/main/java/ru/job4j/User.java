37bd3465c5eb207f0d4136002bef7a0ac4155a01
axelmark <axelmark@mail.ru>
2022-03-12 17:10:48 +0300

    1. Создать модель User [#999]

--- src/main/java/ru/job4j/User.java
+++ src/main/java/ru/job4j/User.java
@@ -4,9 +4,9 @@ import java.util.Calendar;
 
 public class User {
 
-    String name;
-    int children;
-    Calendar birthday;
+    private String name;
+    private int children;
+    private Calendar birthday;
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
