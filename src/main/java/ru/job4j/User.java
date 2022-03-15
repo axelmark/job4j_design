4a4d01b05dfde7fc7f9d0bd99a12d3011bf6b66d
axelmark <axel@mail.ru>
2022-03-15 13:34:30 +0300

    5. Переопределить и equals и hashCode [#1002]

--- src/main/java/ru/job4j/User.java
+++ src/main/java/ru/job4j/User.java
@@ -27,8 +27,8 @@ public class User {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) { return true; }
+        if (o == null || getClass() != o.getClass()) { return false; }
         User user = (User) o;
         return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
     }
