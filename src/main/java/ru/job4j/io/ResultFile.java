1901e600f952c6c206f751c8da212ec5d1fccb1f
axelmark <axelmark@outlook.com>
2023-11-29 13:30:39 +0300

    2.0. FileOutputStream [#252488 #463154]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,13 +1,14 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
             out.write("Hello, world!".getBytes());
             out.write(System.lineSeparator().getBytes());
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
