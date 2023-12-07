90fd73c7f6d76835746df8c23052fa72f9e08a96
axelmark <axelmark@outlook.com>
2023-12-07 19:45:00 +0300

    7. Анализ доступности сервера [#859 #464461]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -5,20 +5,26 @@ import java.io.*;
 public class Analysis {
     public void unavailable(String source, String target) {
         boolean server = false;
+        StringBuilder str = new StringBuilder();
         try (BufferedReader read = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(target)) {
             for (String s : read.lines().toList()) {
                 String[] st = s.split(" ");
                 if (Integer.parseInt(st[0]) == 400 || Integer.parseInt(st[0]) == 500) {
                     if (!server) {
-                        out.write(st[1] + ";");
+                        str.append(st[1]);
+                        str.append(";");
+                        out.write(String.valueOf(str));
                         server = true;
                     }
                 } else if (server) {
-                    out.write(st[1] + ";");
+                    str.append(st[1]);
+                    str.append(";");
+                    out.write(String.valueOf(str));
                     out.write(System.lineSeparator());
                     server = false;
                 }
+                str = new StringBuilder();
             }
         } catch (IOException e) {
             e.printStackTrace();
