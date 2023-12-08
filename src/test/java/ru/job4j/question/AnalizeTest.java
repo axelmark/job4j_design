b68cda5e522cf19a437da8b557d9dc61303d7b3a
axelmark <axelmark@outlook.com>
2023-12-08 04:25:10 +0300

    3. Статистика по коллекции. [#45889 #463816]

--- src/test/java/ru/job4j/question/AnalizeTest.java
+++ src/test/java/ru/job4j/question/AnalizeTest.java
@@ -18,15 +18,15 @@ class AnalizeTest {
         assertThat(Analize.diff(previous, current)).isEqualTo(new Info(0, 0, 0));
     }
 
-//    @Test
-//    void whenOneChanged() {
-//        User u1 = new User(1, "A");
-//        User u2 = new User(2, "B");
-//        User u3 = new User(3, "C");
-//        Set<User> previous = Set.of(u1, u2, u3);
-//        Set<User> current = Set.of(u1, new User(2, "BB"), u3);
-//        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(0, 1, 0));
-//    }
+ /*   @Test
+    void whenOneChanged() {
+        User u1 = new User(1, "A");
+        User u2 = new User(2, "B");
+        User u3 = new User(3, "C");
+        Set<User> previous = Set.of(u1, u2, u3);
+        Set<User> current = Set.of(u1, new User(2, "BB"), u3);
+        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(0, 1, 0));
+    }*/
 
     @Test
     void whenOneDeleted() {
@@ -57,5 +57,4 @@ class AnalizeTest {
         Set<User> current = Set.of(new User(1, "AA"), u2, new User(4, "D"));
         assertThat(Analize.diff(previous, current)).isEqualTo(new Info(1, 1, 1));
     }
-
 }
