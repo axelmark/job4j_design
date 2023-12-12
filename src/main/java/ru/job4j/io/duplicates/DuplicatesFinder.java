6e66ccd0d9f8f3df913addcc4cfc939008c05165
axelmark <axelmark@outlook.com>
2023-12-13 00:29:57 +0300

    11. Поиск дубликатов [#315066 #466023]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,9 +3,18 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
+        DuplicatesVisitor d = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("./"), d);
+        List<Path> duplicates = d.sortDuplicates(d.basic);
+        System.out.println("Найдено дубликатов: " + duplicates.size());
+        duplicates.forEach(x -> System.out.printf("Имя: %s - Размер: %s - Путь: %s \n",
+                x.toFile().getName(),
+                x.toFile().length(),
+                x.toFile().getAbsolutePath()
+        ));
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,18 +5,27 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private final Set<FileProperty> set = new HashSet<>();
-    private final List<String> duplicates = new ArrayList<>();
+    public final List<Path> basic = new ArrayList<>();
+    public final Set<FileProperty> elements = new HashSet<>();
+
+    List<Path> sortDuplicates(List<Path> list) {
+        return list.stream()
+                .filter(f -> !elements.add(new FileProperty(f.toFile().length(), f.toFile().getName())))
+                .collect(Collectors.toList());
+    }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (file.toFile().isFile() && !set.add(new FileProperty(file.toFile().length(), file.toFile().getName()))) {
-            duplicates.add(file.toFile().getAbsolutePath());
+        if (attrs.isRegularFile()) {
+            basic.add(file);
         }
-        duplicates.forEach(System.out::println);
         return super.visitFile(file, attrs);
     }
 }
