31fcd46ebcf930fde502b10d18da1a30ecfcf602
axelmark <axelmark@outlook.com>
2023-12-14 06:09:26 +0300

    2. Схема прав пользователи и роли. [#1093]

---
+++ db/2. Схема прав пользователи и роли. [#1093]/create.sql
@@ -0,0 +1,51 @@
+crete table users (
+    id serial primary key,
+    user_name varchar(255)
+);
+
+create table roles (
+    id serial primary key,
+    role_name varchar(255),
+    user_id int references users(id)
+);
+
+crete table rules (
+    id serial primary key,
+    rule_name varchar(255)
+);
+
+create table roles_rules (
+    id serial primary key,
+    roles_id int references roles(id),
+    rules_id int references rules(id)
+);
+
+create table items (
+    id serial primary key,
+    items_name varchar(255),
+    users_id int references users(id)
+);
+
+create table comments (
+    id serial primary key,
+    comments varchar(255),
+    items_id int references items(id)
+);
+
+create table attachs (
+    id serial primary key,
+    files varchar(255),
+    items_id int references items(id)
+);
+
+create table categories (
+    id serial primary key,
+    cat_name varchar(255),
+    items_id int references items(id)
+);
+
+create table states (
+    id serial primary key,
+    states varchar(255),
+    items_id int references items(id)
+);
---
+++ db/2. Схема прав пользователи и роли. [#1093]/insert.sql
@@ -0,0 +1,8 @@
+insert into users (user_name) values ('Alexey');
+insert into roles (role_name) values ('admin');
+insert into rules (rule_name) values ('all');
+insert into items (items_name) values ('Refactor code');
+insert into comments (comments) values ('comments ....');
+insert into attachs (files) values ('c:/files.doc');
+insert into categories (cat_name) values ('urgent!');
+insert into states (states) values ('waiting...');
