0964c040bbf7ee6666a5f523d2cfb1874b8bb183
axelmark <axelmark@outlook.com>
2023-12-17 12:57:20 +0300

    11. Поиск дубликатов [#315066 #466023]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -18,12 +18,11 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty entries = new FileProperty(file.toFile().length(), file.toFile().getName());
         String path = file.toFile().getAbsolutePath();
-        List<String> tempList = new ArrayList<>();
-        if (elements.containsKey(entries)) {
-            tempList = elements.get(entries);
-        }
-        tempList.add(path);
-        elements.putIfAbsent(entries, tempList);
+
+       /* elements.putIfAbsent(entries, new ArrayList<>());
+        elements.get(entries).add(path);*/
+
+        elements.computeIfAbsent(entries, k -> new ArrayList<>()).add(path);
         return super.visitFile(file, attrs);
     }
 }
