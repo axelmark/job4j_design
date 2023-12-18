2db2e681fafc419600950352b6bc4ae19c2b5797
axelmark <axelmark@outlook.com>
2023-12-18 16:19:55 +0300

    some updates

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -19,7 +19,7 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
         FileProperty entries = new FileProperty(file.toFile().length(), file.toFile().getName());
         String path = file.toFile().getAbsolutePath();
 
-       /* elements.putIfAbsent(entries, new ArrayList<>());
+        /*elements.putIfAbsent(entries, new ArrayList<>());
         elements.get(entries).add(path);*/
 
         elements.computeIfAbsent(entries, k -> new ArrayList<>()).add(path);
