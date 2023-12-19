94fa26f22f5563cd9e5a1e9e507a09362bad2050
axelmark <axelmark@outlook.com>
2023-12-19 14:00:44 +0300

    14. Архивировать проект [#861 #467246]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,19 +1,19 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
     public void packFiles(List<Path> sources, File target) {
-        Map<List<Path>, File> map = new HashMap<>();
-        map.putIfAbsent(sources, target);
+        for (Path s : sources) {
+            packSingleFile(s.toFile(), target);
+        }
     }
 
     public void packSingleFile(File source, File target) {
@@ -27,20 +27,29 @@ public class Zip {
         }
     }
 
+    private void validate(String... args) {
+        if (!Files.exists(Path.of(args[0])) && Files.isDirectory(Path.of(args[0]))) {
+            throw new IllegalArgumentException("Directory doesn't exist");
+        }
+        if (!args[1].matches("^[a-z]*.zip")) {
+            throw new IllegalArgumentException("The file extension must match the template '.zip' ");
+        }
+    }
+
     public static void main(String[] args) {
-        String[] arguments = new String[]{"-d=/Volumes/Project/JAVA/job4j_design", "-e=.class", "-o=project.zip"};
+        String[] arguments = new String[]{"-d=pom.xml", "-e=.class", "-o=pom.zip"};
         ArgsName argsName = ArgsName.of(arguments);
-        Search.validate(argsName.get("d"), argsName.get("e"));
 
         Zip zip = new Zip();
+        zip.validate(argsName.get("d"), argsName.get("o"));
 
         var temp = new ArrayList<Path>();
-        temp.add(new File("./pom.xml").toPath());
-        zip.packFiles(temp, new File("./pom.zip"));
+        temp.add(Path.of(argsName.get("d")));
+        zip.packFiles(temp, new File(argsName.get("o")));
 
-        zip.packSingleFile(
+      /*  zip.packSingleFile(
                 new File("./pom.xml"),
                 new File("./pom.zip")
-        );
+        );*/
     }
 }
