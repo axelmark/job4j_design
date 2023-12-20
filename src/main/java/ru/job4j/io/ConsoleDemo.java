3a3f4cfb01c23d90c1af35d49841d0bc7aa2d945
axelmark <axelmark@outlook.com>
2023-12-20 14:34:39 +0300

    18. Класс Console [#504991]

---
+++ src/main/java/ru/job4j/io/ConsoleDemo.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import java.io.Console;
+import java.util.Arrays;
+
+public class ConsoleDemo {
+    /**
+     * запуск с консоли
+     * java ConsoleDemo.java
+     * java -Dfile.encoding=UTF8 ConsoleDemo.java
+     */
+    public static void main(String[] args) {
+        String login;
+        char[] charPassword;
+        Console console = System.console();
+        if (console == null) {
+            System.out.println("Консоль недоступна");
+            return;
+        }
+        login = console.readLine("%s", "Введите логин: ");
+        console.printf("Ваш логин: %s\n", login);
+        charPassword = console.readPassword("%s", "Введите пароль: ");
+        System.out.println("Ваш пароль: " + String.valueOf(charPassword));
+        Arrays.fill(charPassword, ' ');
+    }
+
+}
