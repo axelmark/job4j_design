3c164a119537047dca546efc391934c80f481d2e
axelmark <axelmark@outlook.com>
2023-12-20 20:50:18 +0300

    2. Схема прав пользователи и роли. [#1093 #466422]

--- db/2. Схема прав пользователи и роли. [#1093]/create.sql
+++ db/2. Схема прав пользователи и роли. [#1093]/create.sql
@@ -1,16 +1,14 @@
---users - roles = many-to-one (у пользователя есть роли)
-create table users (
-    id serial primary key,
-    user_name varchar(255)
-);
-
 create table roles (
     id serial primary key,
-    role_name varchar(255),
-    user_id int references users(id)
+    role_name varchar(255)
 );
 
---roles - rules = many-to-many (у ролей есть права)
+create table users (
+    id serial primary key,
+    user_name varchar(255),
+    roles_id int references roles(id)
+);
+
 create table rules (
     id serial primary key,
     rule_name varchar(255)
@@ -22,37 +20,33 @@ create table roles_rules (
     rules_id int references rules(id)
 );
 
---items - users = many-to-one (у пользователя есть заявки)
 create table items (
     id serial primary key,
     items_name varchar(255),
-    users_id int references users(id)
+    users_id int references users(id),
+    categories_id int references categories(id),
+    states_id int references states(id)
+
 );
 
---items - comments = one-to-many (у заявки есть комментарии)
 create table comments (
     id serial primary key,
     comments varchar(255),
     items_id int references items(id)
 );
 
---items - attachs = one-to-many (у заявки есть приложенные файлы)
 create table attachs (
     id serial primary key,
     files varchar(255),
     items_id int references items(id)
 );
 
---items - categories = many-to-one (у заявки есть категории)
 create table categories (
     id serial primary key,
-    cat_name varchar(255),
-    items_id int references items(id)
+    cat_name varchar(255)
 );
 
---items - states = many-to-one (у заявки есть состояния)
 create table states (
     id serial primary key,
-    states varchar(255),
-    items_id int references items(id)
+    states varchar(255)
 );
