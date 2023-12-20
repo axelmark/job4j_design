c7d83bd86ddae2bbe030f07225ee100a9598a4a8
axelmark <axelmark@outlook.com>
2023-12-20 18:31:55 +0300

    14. Архивировать проект [#861 #467246]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -3,16 +3,21 @@ package ru.job4j.io;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-
     public void packFiles(List<Path> sources, File target) {
-        for (Path s : sources) {
-            packSingleFile(s.toFile(), target);
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path s : sources) {
+                zip.putNextEntry(new ZipEntry(s.toFile().getAbsolutePath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(s.toFile().getAbsolutePath()))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -27,27 +32,26 @@ public class Zip {
         }
     }
 
-    private void validate(String... args) {
-        if (!Files.exists(Path.of(args[0])) && Files.isDirectory(Path.of(args[0]))) {
+    private void validate(ArgsName argsName) {
+        if (!Files.exists(Path.of(argsName.get("d"))) && Files.isDirectory(Path.of(argsName.get("o")))) {
             throw new IllegalArgumentException("Directory doesn't exist");
         }
-        if (!args[1].matches("^[a-z]*.zip")) {
+        if (!argsName.get("o").endsWith(".zip")) {
             throw new IllegalArgumentException("The file extension must match the template '.zip' ");
         }
     }
 
-    public static void main(String[] args) {
-        String[] arguments = new String[]{"-d=pom.xml", "-e=.class", "-o=pom.zip"};
-        ArgsName argsName = ArgsName.of(arguments);
-
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Arguments not passed to program");
+        }
+        ArgsName argsName = ArgsName.of(args);
         Zip zip = new Zip();
-        zip.validate(argsName.get("d"), argsName.get("o"));
+        zip.validate(argsName);
+        List<Path> listCat = Search.search(Path.of(argsName.get("d")), p -> !p.toFile().getName().endsWith(argsName.get("e")));
+        zip.packFiles(listCat, new File(argsName.get("o")));
 
-        var temp = new ArrayList<Path>();
-        temp.add(Path.of(argsName.get("d")));
-        zip.packFiles(temp, new File(argsName.get("o")));
-
-      /*  zip.packSingleFile(
+         /*  zip.packSingleFile(
                 new File("./pom.xml"),
                 new File("./pom.zip")
         );*/
