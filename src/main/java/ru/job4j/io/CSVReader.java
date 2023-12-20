30ca1b13cce944c21e53a22d9345be81e9cc8857
axelmark <axelmark@outlook.com>
2023-12-20 14:00:44 +0300

    17. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -3,6 +3,8 @@ package ru.job4j.io;
 import java.io.File;
 import java.util.Scanner;
 
+import static java.lang.String.format;
+
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
         Scanner scanner = new Scanner(new File(argsName.get("path")))
@@ -19,14 +21,23 @@ public class CSVReader {
 
     public static void main(String[] args) throws Exception {
         String[] arguments = new String[]{"-path=data/csvreader/file.csv", "-delimiter=;", "-out=stdout", "-filter=name,age"};
+
+        if (arguments.length == 0) {
+            throw new IllegalArgumentException("Arguments not passed to program");
+        }
         for (String s : arguments) {
-            if (!s.isBlank() && s.contains("=")) {
-                String[] entries = s.split("=");
-                for (String st : entries) {
-                    if (st.isBlank()) {
-                        throw new IllegalArgumentException();
-                    }
-                }
+            if (!s.contains("=")) {
+                throw new IllegalArgumentException(format("Error: This argument '%s' does not contain an equal sign", s));
+            }
+            if (!s.startsWith("-")) {
+                throw new IllegalArgumentException(format("Error: This argument '%s' does not start with a '-' character", s));
+            }
+            String[] temp = s.split("=", 2);
+            if (temp[0].replaceAll("-", "").trim().isBlank()) {
+                throw new IllegalArgumentException(format("Error: This argument '%s' does not contain a key", s));
+            }
+            if (temp[1].isBlank()) {
+                throw new IllegalArgumentException(format("Error: This argument '%s' does not contain a value", s));
             }
         }
 
