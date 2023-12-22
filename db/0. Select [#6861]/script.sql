5169db9c25f1e6d187c707671791aa9363e0e92f
axelmark <axelmark@outlook.com>
2023-12-22 23:45:15 +0300

    0. Select [#6861 #467719]

--- db/0. Select [#6861]/script.sql
+++ db/0. Select [#6861]/script.sql
@@ -16,5 +16,3 @@ select * from fauna where name like '%fish%';
 select * from fauna where avg_age > 10000 and avg_age < 21000;
 select * from fauna where discovery_date is null;
 select * from fauna where discovery_date < '1950-01-01'::timestamp;
-
-
