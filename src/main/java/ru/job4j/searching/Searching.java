f9afd8d572af7b7bb88e13bb35ee1ea3f23038e2
axelmark <axelmark@outlook.com>
2023-12-22 23:37:15 +0300

    3. Поиск файлов по критерию [#783 #467794]

---
+++ src/main/java/ru/job4j/searching/Searching.java
@@ -0,0 +1,51 @@
+package ru.job4j.searching;
+
+import ru.job4j.io.ArgsName;
+import ru.job4j.io.Search;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+
+public class Searching {
+    public void start(ArgsName argsName) throws IOException {
+
+        String pattern = "";
+        if (argsName.get("t").equals("name")) {
+            pattern = "(.*)" + argsName.get("n") + "(.*)";
+        }
+        String finalPattern = pattern;
+        List<Path> res = Search.search(Path.of(argsName.get("d")), p -> p.toFile().getName().matches(finalPattern));
+
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(String.valueOf(argsName.get("o"))))) {
+            for (Path s : res) {
+                out.write(s.toString().getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Файл записан " + argsName.get("o"));
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите директорию, в которой начинать поиск");
+        String d = scanner.next();
+        System.out.println("Введите имя файла, маска, либо регулярное выражение.");
+        String n = scanner.next();
+        System.out.println("Введите тип поиска: mask искать по маске, name по полному совпадение имени, regex по регулярному выражению.");
+        String t = scanner.next();
+        System.out.println("Введите результат записать в файл");
+        String o = scanner.next();
+        String set = String.format("-d=%s -n=%s -t=%s -o=%s", d, n, t, o);
+        ArgsName argsName = ArgsName.of(set.split(" "));
+
+//        ArgsName argsName = ArgsName.of(new String[]{"-d=.", "-n=.txt", "-t=name", "-o=data/searching/log.txt"});
+        Searching search = new Searching();
+        search.start(argsName);
+    }
+}
