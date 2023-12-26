bf0cf262b10f5151b3ed6306a5d98980c1dcff92
axelmark <axelmark@outlook.com>
2023-12-26 16:40:45 +0300

    2. Группировка и агрегатные функции [#373618 #468238]

--- db/2. Группировка и агрегатные функции [#373618]/scripts.sql
+++ db/2. Группировка и агрегатные функции [#373618]/scripts.sql
@@ -1,19 +1,19 @@
---create table devices(
---    id serial primary key,
---    name varchar(255),
---    price float
---);
---
---create table people(
---    id serial primary key,
---    name varchar(255)
---);
+create table devices(
+    id serial primary key,
+    name varchar(255),
+    price float
+);
 
---create table devices_people(
---    id serial primary key,
---    device_id int references devices(id),
---    people_id int references people(id)
---);
+create table people(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table devices_people(
+    id serial primary key,
+    device_id int references devices(id),
+    people_id int references people(id)
+);
 
 insert into people(name) values ('Alexey');
 insert into people(name) values ('Kate');
