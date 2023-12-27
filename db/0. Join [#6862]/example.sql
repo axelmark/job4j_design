8578d7f2de0cf4de5825c50e2203bf721781d173
axelmark <axelmark@outlook.com>
2023-12-27 19:24:23 +0300

    example update

--- db/0. Join [#6862]/example.sql
+++ db/0. Join [#6862]/example.sql
@@ -1,11 +1,11 @@
 create table owners(
-    id   serial primary key,
+    id serial primary key,
     name varchar(255)
 );
 
 create table devices(
-    id       serial primary key,
-    name     varchar(255),
+    id serial primary key,
+    name varchar(255),
     owner_id int references owners (id)
 );
 
