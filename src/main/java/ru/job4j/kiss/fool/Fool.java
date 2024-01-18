9e302baa9db65e319e32efef2a1b8da32b24a371
axelmark <axelmark@outlook.com>
2024-01-18 15:36:26 +0300

    2. Принципы Kiss, Dry и Yagni [#238813]

--- src/main/java/ru/job4j/kiss/fool/Fool.java
+++ src/main/java/ru/job4j/kiss/fool/Fool.java
@@ -7,35 +7,49 @@ public class Fool {
 
     public static void main(String[] args) {
         System.out.println("Игра FizzBuzz.");
+
         var input = new Scanner(System.in);
         while (startAt < 100) {
             if (startAt % 3 == 0 && startAt % 5 == 0) {
                 System.out.println("FizzBuzz");
-            }
-            if (startAt % 3 == 0) {
+            } else if (startAt % 3 == 0) {
                 System.out.println("Fizz");
-            }
-            if (startAt % 5 == 0) {
+            } else if (startAt % 5 == 0) {
                 System.out.println("Buzz");
             } else {
                 System.out.println(startAt);
             }
             startAt++;
             var answer = input.nextLine();
-            if (startAt % 3 == 0 && startAt % 5 == 0 && !"FizzBuzz".equals(answer)
-                    || startAt % 3 == 0 && !"Fizz".equals(answer) || startAt % 5 == 0 && !"Buzz".equals(answer)) {
-                showMessageAndStartAt0();
+            if (isFizzBuzz()) {
+                if (!"FizzBuzz".equals(answer)) {
+                    System.out.println("Ошибка. Начинай снова.");
+                    startAt = 0;
+                }
+            } else if (startAt % 3 == 0) {
+                if (!"Fizz".equals(answer)) {
+                    System.out.println("Ошибка. Начинай снова.");
+                    startAt = 0;
+                }
+            } else if (startAt % 5 == 0) {
+                if (!"Buzz".equals(answer)) {
+                    System.out.println("Ошибка. Начинай снова.");
+                    startAt = 0;
+                }
             } else {
                 if (!String.valueOf(startAt).equals(answer)) {
-                    showMessageAndStartAt0();
+                    System.out.println("Ошибка. Начинай снова.");
+                    startAt = 0;
                 }
             }
             startAt++;
         }
     }
 
-    private static void showMessageAndStartAt0() {
-        System.out.println("Ошибка. Начинай снова.");
-        startAt = 0;
+    public static boolean isFizzBuzz() {
+        if (startAt % 3 == 0 && startAt % 5 == 0) {
+            return true;
+        }
+        return false;
     }
 }
