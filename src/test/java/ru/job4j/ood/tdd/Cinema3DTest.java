a84f2ed71a6cb0b59c2f996bb6d3a3c47fa03ab2
axelmark <axelmark@outlook.com>
2024-01-26 20:16:22 +0300

    3. Что такое TDD? [#4918 #474689]

---
+++ src/test/java/ru/job4j/ood/tdd/Cinema3DTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.ood.tdd;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
+public class Cinema3DTest {
+    @Test
+    public void whenBuyThenGetTicket() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket).isEqualTo(new Ticket3D());
+    }
+
+    @Test
+    public void whenAddSessionThenItExistsBetweenAllSessions() {
+        Cinema cinema = new Cinema3D();
+        Session session = new Session3D();
+        cinema.add(session);
+        List<Session> sessions = cinema.find(data -> true);
+        assertThat(sessions).contains(session);
+    }
+
+    @Test
+    public void whenBuyOnInvalidRowThenGetException() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        assertThatThrownBy(() -> cinema.buy(account, -1, 1, date))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
